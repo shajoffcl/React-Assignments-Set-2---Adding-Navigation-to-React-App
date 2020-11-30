b3604b67d848eb4b62bc67f89b150f80a3e649bc
Mohd Shaib <shaj.native@gmail.com>
2020-11-30 18:50:31 +0530

    updated 0.7

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,18 @@ function Error() {
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route exact path="/" component={Home} />
-          <Route path="/" component={Error} />
-        </Switch>
-        <LocationDisplayComponent />
-      </div>
+      <>
+        <div id="main">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route path="/" component={Error} />
+          </Switch>
+          <LocationDisplayComponent />
+        </div>
+      </>
     );
   }
 }

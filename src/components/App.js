44eceb53954d7f03a0feeb6ea92e4590df500cd4
Mohd Shaib <shaj.native@gmail.com>
2020-11-30 17:13:50 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <div id="nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+import { BrowserRouter } from "react-router-dom";
 
-
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
